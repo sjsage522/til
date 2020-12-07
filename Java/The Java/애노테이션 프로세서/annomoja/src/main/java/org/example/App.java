142d2282d66259390ebcc5bd984ac24cc93c9796
shinjunseok <sjsage522@gmail.com>
2020-12-08 08:09:12 +0900

    upload: annotation processor project
    애노테이션 프로세서 실습 프로젝트

---
+++ Java/The Java/애노테이션 프로세서/annomoja/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Moja moja = new MagicMoja();
+        System.out.println(moja.pullOut());
+    }
+}
---
+++ Java/The Java/애노테이션 프로세서/annomoja/src/main/java/org/example/Moja.java
@@ -0,0 +1,6 @@
+package org.example;
+
+@Magic
+public interface Moja {
+    String pullOut();
+}
